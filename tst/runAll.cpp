e02f59da9bdc2a4d8e5930dc48c7c5bc873c5f8b
lianzeng <liang.1.zeng@nokia.com>
2016-11-16 13:40:48 +0800

    solve makefile error

--- tst/runAll.cpp
+++ tst/runAll.cpp
@@ -1,24 +1,28 @@
 #include <gtest/gtest.h>
 
 
-class TestGame : public ::testing::Test{
+class TestGame : public ::testing::Test
+{
 public:
-  void SetUp(){
+  void SetUp()
+  {
     //code here be executed before test case
   }
 
-  void TearDown(){
+  void TearDown()
+  {
     //Code here be executed after test case
   }
 };
 
 TEST(TestGame, startFromHere)
 {
-  FAIL()<<"start from here!";
+  //FAIL()<<"start from here!";
 }
 
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
   ::testing::InitGoogleTest(&argc, argv);
   
   return RUN_ALL_TESTS();
